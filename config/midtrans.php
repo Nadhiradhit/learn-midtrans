fbaa4bbee7e97e1bcaff28ea410a3a3e90e26a7a
Nadhiradhit <nadhiradhitya@gmail.com>
2023-10-28 12:22:22 +0700

    feat: add midtrans

---
+++ config/midtrans.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID', 'learn_midtrans'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'learn_midtrans'),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'learn_midtrans'),
+];
+
+?>
